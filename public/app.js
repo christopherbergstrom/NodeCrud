998f5c7c2fa260f8898e691dff8e41b5ee9027f9
Christopher Bergstrom <christopherbergstrom@Christophers-MacBook-Pro.local>
2016-03-15 11:05:02 -0600

    init commit

---
+++ public/app.js
@@ -0,0 +1,235 @@
+window.onload = function()
+{
+  console.log("in onload");
+  document.getElementById("button").addEventListener("click", function()
+  {
+    // getData("/getAll", createList);
+    checkAndRemove();
+    getData("/getAll");
+  });
+
+  // var item = document.addForm.addItem.value;
+  // var obj = {"buyer":buyer, "item":item, "tracking":tracking};
+  // updateData("POST", "rest/data", obj);
+
+  document.myForm.submit.addEventListener("click", function(e)
+  {
+    checkAndRemove();
+    e.preventDefault();
+    var pass = validate();
+    if (pass)
+    {
+      console.log("in if pass");
+      var item = document.myForm.send.value;
+      var obj = {"item":item};
+      document.myForm.reset();
+      verbData("POST", "/add", undefined, obj);
+    }
+    // {
+    //   document.getElementById("content").innerHTML = data;
+    // }, {data : document.myForm.send.value});
+  });
+};
+
+var body = document.querySelector("body");
+var selectDiv = document.getElementById("selectDiv");
+var id;
+
+function checkAndRemove()
+{
+  resultsTable = document.getElementById("resultsTable");
+  if(resultsTable)
+  {
+    resultsTable.parentNode.removeChild(resultsTable);
+  }
+  updateButton = document.getElementById("updateButton");
+  if(updateButton)
+  {
+    updateButton.parentNode.removeChild(updateButton);
+  }
+  updateForm = document.getElementById("updateForm");
+  if(updateForm)
+  {
+    updateForm.parentNode.removeChild(updateForm);
+  }
+  updateButton = document.getElementById("updateButton");
+  if(updateButton)
+  {
+    updateButton.parentNode.removeChild(updateButton);
+  }
+  deleteButton = document.getElementById("deleteButton");
+  if(deleteButton)
+  {
+    deleteButton.parentNode.removeChild(deleteButton);
+  }
+  updateTable = document.getElementById("updateTable");
+  if(updateTable)
+  {
+    updateTable.parentNode.removeChild(updateTable);
+  }
+}
+
+function validate()
+{
+  console.log("in validate");
+  var item = document.myForm.send.value;
+  console.log(item);
+  if (item === "")
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+  console.log(item);
+}
+
+function getData(url, callback)
+{
+  var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+
+	xhr.onreadystatechange = function() {
+		if (xhr.status < 400 && xhr.readyState == 4) {
+			console.log(xhr.responseText);
+			var array = JSON.parse(xhr.responseText);
+      createList(array);
+		}
+	};
+
+	xhr.send(null);
+}
+
+function createList(array)
+{
+  var content = document.getElementById("content").innerHTML;
+  var resultsTable = document.createElement("table");
+  resultsTable.setAttribute("id", "resultsTable");
+  var tr = document.createElement("tr");
+  var th = document.createElement("th");
+  th.innerHTML = "Item";
+  tr.appendChild(th);
+  resultsTable.appendChild(tr);
+  var radioButtonsArray = [];
+  for (var i = 0; i < array.length; i++)
+  {
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+    var item = array[i].item;
+    td.innerHTML = array[i].item;
+    tr.appendChild(td);
+    var td = document.createElement("td");
+    var radio = document.createElement("input");
+    radio.setAttribute("type", "radio");
+    radio.setAttribute("name", "radioButton");
+    radio.setAttribute("value", array[i].id);
+    radioButtonsArray.push(radio);
+    td.appendChild(radio);
+    tr.appendChild(td);
+    resultsTable.appendChild(tr);
+  }
+  body.appendChild(resultsTable);
+  var updateButton = document.createElement("button");
+  updateButton.setAttribute("id", "updateButton");
+  updateButton.innerHTML = "update";
+  updateButton.setAttribute("class", "buttons");
+  body.appendChild(updateButton);
+  var deleteButton = document.createElement("button");
+  deleteButton.setAttribute("id", "deleteButton");
+  deleteButton.innerHTML = "delete";
+  deleteButton.setAttribute("class", "buttons");
+  body.appendChild(deleteButton);
+  deleteButton.addEventListener("click", function()
+  {
+    checkAndRemove();
+    for (var j = 0; j < radioButtonsArray.length; j++)
+    {
+      if (radioButtonsArray[j].checked === true)
+      {
+        id = radioButtonsArray[j].value;
+        console.log(id);
+        verbData("DELETE", "/del/"+id);
+      }
+    }
+  });
+  updateButton.addEventListener("click", function()
+  {
+    checkAndRemove();
+    for (var j = 0; j < radioButtonsArray.length; j++)
+    {
+      if (radioButtonsArray[j].checked === true)
+      {
+        id = radioButtonsArray[j].value;
+        console.log(id);
+        for (var k = 0; k < array.length; k++)
+        {
+          var updateForm = document.createElement("form");
+          updateForm.setAttribute("name", "updateForm");
+          updateForm.setAttribute("id", "updateForm");
+          var updateItem = document.createElement("input");
+          updateItem.setAttribute("name", "updateItem");
+          updateItem.setAttribute("type", "text");
+          updateItem.setAttribute("value", array[j].item);
+          updateForm.appendChild(updateItem);
+          var updateSubmit = document.createElement("input");
+          updateSubmit.setAttribute("type", "submit");
+          updateSubmit.setAttribute("name", "updateSubmit");
+          updateSubmit.setAttribute("value", "update");
+          updateSubmit.setAttribute("class", "buttons");
+          updateForm.appendChild(updateSubmit);
+          checkAndRemove();
+          var updateTable = document.createElement("table");
+          updateTable.setAttribute("id", "updateTable");
+          var tr = document.createElement("tr");
+          var th = document.createElement("th");
+          th.innerHTML = "Updating Item";
+          tr.appendChild(th);
+          updateTable.appendChild(tr);
+          body.appendChild(updateTable);
+          body.appendChild(updateForm);
+
+          updateForm.updateSubmit.addEventListener("click", function(e)
+          {
+            e.preventDefault();
+            var item = document.updateForm.updateItem.value;
+            var newObj = {"item":item};
+            checkAndRemove();
+            // id++;
+            console.log(id);
+            verbData("PUT", "/send/"+id, undefined, newObj);
+          });
+        }
+      }
+    }
+  });
+}
+
+function verbData(method, url, callback, obj) {
+  console.log(method);
+  console.log(url);
+  console.log(obj);
+  // console.log(id);
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	if (obj) {
+		xhr.setRequestHeader('Content-Type', 'application/json');
+	}
+
+  xhr.onreadystatechange = function() {
+		if (xhr.status < 400 && xhr.readyState == 4) {
+			console.log(xhr.responseText);
+			if (callback) {
+				callback(JSON.parse(xhr.responseText).data);
+			}
+		}
+	};
+
+	if (obj) {
+    console.log("object");
+		xhr.send(JSON.stringify(obj));
+	} else {
+    console.log("NO object");
+		xhr.send(null);
+	}
+}
---
+++ server.js
@@ -0,0 +1,121 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var credentials = require('./credentials.js');
+app.use(cookieParser(credentials.cookieSecret));
+
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : 'root',
+	database : 'todo_rest'
+});
+
+app.use(bodyParser.json());
+
+// app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(__dirname + '/public'));
+app.set("views", __dirname + "/public/");
+app.engine("html", require ("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get('/', function(req,res) {
+	res.render('index.html');
+});
+
+app.get("/signed", function(req,res)
+{
+	res.cookie("testCookie", {test : "test"}, {signed : true});
+	res.render("index.html");
+});
+
+app.get("/unsigned", function(req,res)
+{
+	res.cookie("unsignedCookie", {test : "test"});
+	res.render("index.html");
+});
+
+app.get("/allCookies", function(req,res)
+{
+	console.log("Cookies: ", req.cookies);
+	Cookies: {unsignedCookie: {test: "test"}}
+});
+
+app.get("/allSignedCookies", function(req,res)
+{
+	console.log("Cookies: ", req.signedCookies);
+	Cookies: {testCookie: {test: "test"}}
+});
+
+app.get("/specificCookie", function(req,res)
+{
+	console.log(req.signedCookies.testCookie);
+	{test: "test"}
+});
+
+app.get("/clearCookie", function(req,res)
+{
+	console.log("Removing cookie: "+req.signedCookies.testCookie);
+	res.clearCookie("testCookie");
+	res.render("index.html");
+});
+
+app.get('/data', function(req,res) {
+	console.log(req.xhr);
+	res.send({data : "Response from the server"});
+});
+
+conn.connect();
+
+app.get('/getAll', function(req,res) {
+    conn.query('SELECT * FROM to_do', function(err,rows,fields) {
+        if (err) {
+            console.log("Something is amiss...");
+            console.log(err);
+        } else {
+            // res.send({data : rows});
+            // console.log({data : rows});
+            res.send(rows);
+            console.log(rows);
+        }
+    });
+});
+
+app.post('/send', function(req,res) {
+	res.send({data : req.body.data + " RESPONSE FROM POST"});
+});
+
+// UPDATE jobs SET minimum_salary = 42000, maximum_salary = 65000 WHERE id = 19;
+// var item = document.updateForm.updateItem.value;
+
+app.put('/send/:id', function(req, res)
+{
+  console.log("in app.put/send");
+  conn.query("UPDATE to_do SET item = '"+req.body.item+"' WHERE id = "+req.params.id);
+});
+
+// DELETE FROM projects WHERE id = 14;
+
+app.delete('/del/:id', function(req, res)
+{
+  console.log("in app.delete/del");
+  conn.query("DELETE FROM to_do WHERE id = "+req.params.id);
+});
+
+// INSERT INTO projects (id, name, start_date) VALUES (14, 'Project X', CURDATE());
+
+app.post('/add', function(req, res)
+{
+  console.log("in app.post/add");
+  conn.query("INSERT INTO to_do (item) VALUES ('"+req.body.item+"')");
+});
+
+app.get('/hello', function(req,res) {
+	res.send('Hello world');
+});
+
+app.listen(3000, function() {
+	console.log("listening on 3000");
+});
